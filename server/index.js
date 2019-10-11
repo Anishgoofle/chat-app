2f2cc23aea61edd5384ac527899c0e1cd2ede6df
Anish Ojha <anishojha82@gmail.com>
2019-10-11 22:08:54 +0530

    added functionality to handle message of user + one common admin message , added chat box css + functionality to send message + added functionality on server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 const PORT = process.env.PORT || 5000;
 const router = require("./router");
 
@@ -10,7 +11,28 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", socket => {
-  console.log("New Connection!");
+  socket.on("join", ({ name, room }, cb) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return cb(error);
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name} welcome to ${user.room}`
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined` });
+    socket.join(user.room);
+    cb();
+  });
+
+  socket.on("sendMessage", (message, cb) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    cb();
+  });
 
   socket.on("disconnect", () => {
     console.log("User left");
