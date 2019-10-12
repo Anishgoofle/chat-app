71bf82fc6e04e4f3546898977a352f187c284bbb
Anish Ojha <anishojha82@gmail.com>
2019-10-12 23:51:11 +0530

    changes + addition of people online

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -5,15 +5,17 @@ import "./Chat.css";
 import Infobar from "../InfoBar/InfoBar";
 import Input from "../Input/Input";
 import Messages from "../Messages/Messages";
+import TextContainer from "../TextContainer/TextContainer";
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://chatbox-be.herokuapp.com/";
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -22,18 +24,26 @@ const Chat = ({ location }) => {
     setName(name);
     setRoom(room);
 
-    socket.emit("join", { name, room }, () => {});
-
-    return () => {
-      socket.emit("disconnect");
-      socket.off();
-    };
+    socket.emit("join", { name, room }, error => {
+      if (error) {
+        alert(error);
+      }
+    });
   }, [ENDPOINT, location.search]);
 
   useEffect(() => {
     socket.on("message", message => {
       setMessages([...messages, message]);
     });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+
+    return () => {
+      socket.emit("disconnect");
+      socket.off();
+    };
   }, [messages]);
 
   //function for sending messages
@@ -57,6 +67,7 @@ const Chat = ({ location }) => {
           sendMessage={sendMessage}
         />
       </div>
+      <TextContainer users={users} />
     </div>
   );
 };
